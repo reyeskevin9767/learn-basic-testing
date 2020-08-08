6ebdaa1838a1a4f086b29bec004f2c77a1a80b2a
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-08 00:45:54 -0400

    using node library

--- index.test.js
+++ index.test.js
@@ -1,3 +1,4 @@
+const assert = require('assert');
 const { forEach, map } = require('./index');
 
 const test = (desc, fn) => {
@@ -15,9 +16,7 @@ test('The forEach function', () => {
     sum += value;
   });
 
-  if (sum !== 6) {
-    throw new Error('Expected summing array to equal 6');
-  }
+  assert.strictEqual(sum, 6, 'Expected forEach to sum the array');
 });
 
 test('The Map Function', () => {
@@ -25,17 +24,7 @@ test('The Map Function', () => {
     return value * 2;
   });
 
-  if (result[0] !== 2) {
-    throw new Error(`Expeceted to find 2, but found  ${result[0]}`);
-  }
-
-  if (result[1] !== 4) {
-    throw new Error(`Expeceted to find 4, but found  ${result[0]}`);
-  }
-
-  if (result[2] !== 6) {
-    throw new Error(`Expeceted to find 6, but found  ${result[0]}`);
-  }
+  assert.deepStrictEqual(result, [2, 4, 6]);
 });
 
 //* 3 Issues without test function
