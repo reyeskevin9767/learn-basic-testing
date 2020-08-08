532f2085fc18b90a2e821746d2de954ad5f39767
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-08 00:52:38 -0400

    add mocha

--- index.test.js
+++ index.test.js
@@ -1,16 +1,17 @@
 const assert = require('assert');
 const { forEach, map } = require('./index');
 
-const test = (desc, fn) => {
-  console.log('----', desc);
-  try {
-    fn();
-  } catch (err) {
-    console.log(err.message);
-  }
-};
+//* Reaplaced with mocha
+// const test = (desc, fn) => {
+//   console.log('----', desc);
+//   try {
+//     fn();
+//   } catch (err) {
+//     console.log(err.message);
+//   }
+// };
 
-test('The forEach function', () => {
+it('The forEach function', () => {
   let sum = 0;
   forEach([1, 2, 3], (value) => {
     sum += value;
@@ -19,7 +20,7 @@ test('The forEach function', () => {
   assert.strictEqual(sum, 6, 'Expected forEach to sum the array');
 });
 
-test('The Map Function', () => {
+it('The Map Function', () => {
   const result = map([1, 2, 3], (value) => {
     return value * 2;
   });
